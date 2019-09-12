b8b63d410368d510310fa179cd3a44d91d84e2a9
Scott Lonis <scott.lonis@outlook.com>
2019-09-12 16:55:26 -0500

    working on page transitions

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -10,7 +10,7 @@ export const PrivateRoute = ({
 }) => (
     <Route {...rest} component={(props) => (
         isAuthenticated ? (
-            <div>
+            <div className="page">
                 <Header />
                 <Component {...props} />
             </div>
